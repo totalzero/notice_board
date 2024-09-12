ff9e36f9be4b50ac77b7b6b5ea39f259e2c7895e
TotalZero <radek.solekr@icloud.com>
2024-09-12 12:08:26 +0200

    added tests to user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+test "testing invalid email length parameter" do
+@user = User.new(email: "bl", password: "password", password_confirmation: "password")
+assert_not @user.valid?
+end
+
+test "test invalid password and password confirmation parameters " do
+@user = User.new(email: "user@domain.com", password: "password", password_confirmation: "pdfsw")
+assert_not @user.valid?
+end
+
+test "test unique email parameter" do
+@user = User.new(email: "example@domain.com", password: "password", password_digest: "password")
+assert_not @user.valid?
+end
 end
